16e379f4890a6473cc09dff77e3d9fce91227e1e
subhanbhatti422-source <subhanbhatti422@gmail.com>
2026-07-24 04:54:14 +0500

    Fix router basename

--- src/main.jsx
+++ src/main.jsx
@@ -3,36 +3,38 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'
-import About from './pages/About/About.jsx'
-import Stores from './pages/Stores/Stores.jsx'
-import Shop from './pages/Shop/Shop.jsx'
 
-const router= createBrowserRouter([
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Stores from './pages/Stores/Stores'
+import Shop from './pages/Shop/Shop'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+    {
+      path: '/stores',
+      element: <Stores />,
+    },
+    {
+      path: '/shop',
+      element: <Shop />,
+    },
+  ],
   {
-    path: '/',
-    element: <Home/>
-  },
-  {
-    path: '/about',
-    element: <About/>
-  },
-  {
-    path: '/stores',
-    element: <Stores/>
-  },
-  {
-    path: '/shop',
-    element: <Shop/>
-  },
-   {
-    basename: "/Kave-Home-react_website",
+    basename: '/Kave-Home-react_website',
   }
-])
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
